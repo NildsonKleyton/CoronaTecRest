420bcea2027b9ea9e9c91d01b883249595c889e5
NildsonKleyton <nildsonkleyton@hotmail.com>
2021-01-29 08:53:10 -0300

    Criado o add e remove contato

--- src/main/java/servico/ClienteServico.java
+++ src/main/java/servico/ClienteServico.java
@@ -1,8 +1,10 @@
 package servico;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -15,16 +17,16 @@ import entidade.RetornoCliente;
 
 @Path("/cliente")
 public class ClienteServico {
-	
+
 	private ClienteDao clienteDAO = new ClienteDAOImpl();
-	
+
 	@GET
 	@Path("/")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String texto() {
 		return "Serviço Cliente";
 	}
-	
+
 	/*
 	 * 
 	 */
@@ -36,22 +38,21 @@ public class ClienteServico {
 		RetornoCliente retorno = new RetornoCliente();
 		retorno.setCodigoRetorno(0);
 		retorno.setMensagemRetorno("Sucesso!!");
-		if(clienteDAO.existeCliente(cliente) == null) {
-			if(cliente.getListaContatos() !=null && cliente.getListaContatos().size() > 0) {
-				for(Contato contato: cliente.getListaContatos()) {
+		if (clienteDAO.existeCliente(cliente) == null) {
+			if (cliente.getListaContatos() != null && cliente.getListaContatos().size() > 0) {
+				for (Contato contato : cliente.getListaContatos()) {
 					contato.setCliente(cliente);
 				}
 			}
 			clienteDAO.inserirCliente(cliente);
-		}else {
+		} else {
 			retorno.setCodigoRetorno(1);
 			retorno.setMensagemRetorno("FALHA: Cliente já exixte!!");
 		}
-		
+
 		return retorno;
 	}
-	
-	
+
 	@GET
 	@Path("/pesquisarCliente")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -63,47 +64,79 @@ public class ClienteServico {
 		retorno.setListaCliente(clienteDAO.pesquisarCliente(cliente));
 		return retorno;
 	}
-	
-	
-//	@Override
-//	public RetornoCleiente adicionarContatoCliente(Cliente cliente) {
-//		RetornoCleiente retorno = new RetornoCleiente();
-//		retorno.setCodigoRetorno(0);
-//		retorno.setMensagemRetorno("Conta");
-//
-//		if (existe(cliente) != null) {
-//			if (jaTem(contato) == null) {
-//				this.listaContato.add(contato);
-//			} else {
-//				retorno.setCodigoRetorno(1);
-//				retorno.setMensagemRetorno("FALHA: Contato já existe!!!");
-//			}
-//		} else {
-//			retorno.setCodigoRetorno(1);
-//			retorno.setMensagemRetorno("FALHA: Não existe cliente!!!");
-//		}
-//		return retorno;
-//
-//	}
-//
-//	@Override
-//	public RetornoCleiente removerContatoCliente(Cliente cliente) {
-//		RetornoCleiente retorno = new RetornoCleiente();
-//		retorno.setCodigoRetorno(0);
-//		retorno.setMensagemRetorno("Contatos adcionado");
-//
-//		if (existe(cliente) != null) {
-//			if (jaTem(contato) != null) {
-//				this.listaContato.remove(contato);
-//			} else {
-//				retorno.setCodigoRetorno(1);
-//				retorno.setMensagemRetorno("FALHA: Contato já existe!!!");
-//			}
-//		} else {
-//			retorno.setCodigoRetorno(1);
-//			retorno.setMensagemRetorno("FALHA: Não existe cliente!!!");
-//		}
-//		return retorno;
-//	}
+
+	@PUT
+	@Path("/adicionarContato")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public RetornoCliente adicionarContatoCliente(Cliente cliente) {
+		RetornoCliente retorno = new RetornoCliente();
+		retorno.setCodigoRetorno(0);
+		retorno.setMensagemRetorno("Sucesso!!");
+		// 1 - verificar se existe o cliente na base
+		Cliente clienteBase = clienteDAO.existeCliente(cliente);
+		if (clienteBase != null) {
+			// 2 - Verificar se existe contatos para serem inseridos
+			if (cliente.getListaContatos() != null && cliente.getListaContatos().size() > 0) {
+				// 3 - Verifica se existe contato
+				boolean existeContato = false;
+				for (Contato contato : cliente.getListaContatos()) {
+					//cria variável "index" para receber o contato 
+					int index = clienteBase.getListaContatos().indexOf(contato);
+					if (index != -1) {
+						existeContato = true;
+					}
+				}
+				// 4 - Senão existe contato adiciona, se existir contato manda o erro,
+				if (!existeContato) {
+					//adiciona o contato
+					clienteBase.getListaContatos().addAll(cliente.getListaContatos());
+				} else {
+					retorno.setCodigoRetorno(1);
+					retorno.setMensagemRetorno("FALHA: Contato já existente!!!");
+				}
+			}
+		} else {
+			retorno.setCodigoRetorno(1);
+			retorno.setMensagemRetorno("FALHA: Cliente não exixte!!");
+		}
+
+		return retorno;
+	}
+
+	@DELETE
+	@Path("/removerContato")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public RetornoCliente removerContatoCliente(Cliente cliente) {
+		RetornoCliente retorno = new RetornoCliente();
+		retorno.setCodigoRetorno(0);
+		retorno.setMensagemRetorno("Sucesso!!");
+		// 1 - verificar se existe o cliente na base
+		Cliente clienteBase = clienteDAO.existeCliente(cliente);
+		if (clienteBase != null) {
+			// 2 - Verificar se existe contatos para serem inseridos
+			if (cliente.getListaContatos() != null && cliente.getListaContatos().size() > 0) {
+				// 3 - Verifica se existe contato
+				for (Contato contato : cliente.getListaContatos()) {
+					//cria variável "index" para receber o contato
+					int index = clienteBase.getListaContatos().indexOf(contato);
+					if (index != -1) {
+						//remove o contato
+						clienteBase.getListaContatos().remove(index);
+					}
+				}
+
+			} else {
+				retorno.setCodigoRetorno(1);
+				retorno.setMensagemRetorno("FALHA: Contato já existente!!!");
+			}
+
+		} else {
+			retorno.setCodigoRetorno(1);
+			retorno.setMensagemRetorno("FALHA: Cliente não exixte!!");
+		}
+		return retorno;
+	}
 
 }
